5363a39331de277c73920d54c859bee91dd8bdfb
noelukwa <ukwauchechukwu@gmail.com>
2023-11-05 21:39:13 +0100

    feat : improve app Response - test  possible cases

--- response.go
+++ response.go
@@ -3,6 +3,7 @@ package coco
 import (
 	"crypto/hmac"
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -13,14 +14,46 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	"strconv"
 	"strings"
 	"time"
 )
 
+// VaryFieldNameRegex checks for valid field names for the Vary header as defined by RFC 7231.
+var VaryFieldNameRegex = regexp.MustCompile(`^[!#$%&'*+\-.^_` + "`" + `|~0-9A-Za-z]+$`)
+
+type DownloadOption struct {
+	MaxAge       int
+	LastModified bool
+	Headers      map[string]string
+	Dotfiles     string
+	AcceptRanges bool
+	CacheControl bool
+	Immutable    bool
+}
+
+var (
+	defaultDownloadOptions = &DownloadOption{
+		MaxAge:       0,
+		LastModified: false,
+		Headers:      nil,
+		Dotfiles:     "ignore",
+		AcceptRanges: true,
+		CacheControl: true,
+		Immutable:    false,
+	}
+	ErrDirPath      = errors.New("specified path is a directory")
+	ErrDotfilesDeny = errors.New("serving dotfiles is not allowed")
+)
+
+func (r *Response) setContentType(contentType string) {
+	if cty := r.w.Header().Get("Content-Type"); cty == "" {
+		r.w.Header().Set("Content-Type", contentType)
+	}
+}
+
 type Response struct {
 	w      http.ResponseWriter
-	ctx    *reqcontext
+	ctx    *context
 	status int
 }
 
@@ -64,56 +97,31 @@ func (r *Response) Cookie(cookie *http.Cookie) *Response {
 
 // SignedCookie SecureCookie sets a signed cookie
 func (r *Response) SignedCookie(cookie *http.Cookie, secret string) *Response {
-
 	mac := hmac.New(sha256.New, []byte(secret))
-	mac.Write([]byte(cookie.Value))
-	mac.Write([]byte(cookie.Name))
-
-	sig := string(mac.Sum(nil))
-	cookie.Value = fmt.Sprintf("%s.%s", sig, cookie.Value)
-
+	_, _ = mac.Write([]byte(cookie.Value))
+	signature := mac.Sum(nil)
+	encodedSignature := base64.StdEncoding.EncodeToString(signature)
+	cookie.Value = fmt.Sprintf("%s.%s", encodedSignature, cookie.Value)
 	http.SetCookie(r.w, cookie)
 	return r
 }
 
 // ClearCookie clears the cookie by setting the MaxAge to -1
-func (r *Response) ClearCookie(cookie *http.Cookie) *Response {
-	cookie.MaxAge = -1
-	return r.Cookie(cookie)
+func (r *Response) ClearCookie(name string) *Response {
+	cookie := &http.Cookie{Name: name, MaxAge: -1}
+	http.SetCookie(r.w, cookie)
+	return r
 }
 
-type DownloadOption struct {
-	MaxAge       int
-	LastModified bool
-	Headers      map[string]string
-	Dotfiles     string
-	AcceptRanges bool
-	CacheControl bool
-	Immutable    bool
-}
-
-var (
-	defaultDownloadOptions = &DownloadOption{
-		MaxAge:       0,
-		LastModified: false,
-		Headers:      nil,
-		Dotfiles:     "ignore",
-		AcceptRanges: true,
-		CacheControl: true,
-		Immutable:    false,
-	}
-	errDirPath      = errors.New("specified path is a directory")
-	errDotfilesDeny = errors.New("serving dotfiles is not allowed")
-)
-
+// Download transfers the file at the given path.
+// Sets the Content-Type response HTTP header field based on the filename’s extension.
 func (r *Response) Download(filepath, filename string, options *DownloadOption, cb func(error)) {
 	if options == nil {
 		options = defaultDownloadOptions
 	}
 
-	// check dotfiles option and update the filename accordingly
 	if options.Dotfiles == "deny" && strings.HasPrefix(filename, ".") {
-		cb(errDotfilesDeny)
+		cb(ErrDotfilesDeny)
 		return
 	}
 
@@ -122,37 +130,50 @@ func (r *Response) Download(filepath, filename string, options *DownloadOption, 
 
 // SendFile transfers the file at the given path.
 // Sets the Content-Type response HTTP header field based on the filename’s extension.
-func (r *Response) SendFile(filepath, filename string, options *DownloadOption, cb func(error)) {
+func (r *Response) SendFile(filePath, fileName string, options *DownloadOption, cb func(error)) {
 	if cb == nil {
-		cb = func(error) {
+		cb = func(err error) {
+			if err != nil {
+				fmt.Printf("Error sending file: %v\n", err)
+			}
 		}
 	}
 
-	fi, err := os.Stat(filepath)
+	if options == nil {
+		options = defaultDownloadOptions
+	}
+
+	fi, err := os.Stat(filePath)
 	if err != nil {
 		cb(err)
 		return
 	}
 
 	if fi.IsDir() {
-		cb(errDirPath)
+		cb(ErrDirPath)
 		return
 	}
 
-	file, err := os.Open(filepath)
+	file, err := os.Open(filePath)
 	if err != nil {
 		cb(err)
 		return
 	}
 	defer file.Close()
 
-	// Construct Cache-Control header
-	cacheControlValues := make([]string, 0)
+	ext := filepath.Ext(fileName)
+	mimeType := mime.TypeByExtension(ext)
+	if mimeType == "" {
+		mimeType = "application/octet-stream"
+	}
+	r.w.Header().Set("Content-Type", mimeType)
+
 	if options.CacheControl {
+		cacheControlValues := []string{"public"}
 		if options.MaxAge > 0 {
-			cacheControlValues = append(cacheControlValues, fmt.Sprintf("public, max-age=%d", options.MaxAge))
+			cacheControlValues = append(cacheControlValues, fmt.Sprintf("max-age=%d", options.MaxAge))
 		} else {
-			cacheControlValues = append(cacheControlValues, "public, max-age=0")
+			cacheControlValues = append(cacheControlValues, "max-age=0")
 		}
 		if options.Immutable {
 			cacheControlValues = append(cacheControlValues, "immutable")
@@ -170,218 +191,170 @@ func (r *Response) SendFile(filepath, filename string, options *DownloadOption, 
 		r.w.Header().Set(key, value)
 	}
 
-	r.w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%q", filename))
+	encodedFilename := url.PathEscape(fileName)
+	r.w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename*=UTF-8''%s", encodedFilename))
 
-	http.ServeContent(r.w, r.ctx.request(), filename, fi.ModTime(), file)
+	http.ServeContent(r.w, r.ctx.request(), fileName, fi.ModTime(), file)
 
 	cb(nil)
-
 }
 
 // JSON sends a JSON response with the given payload.
 func (r *Response) JSON(v interface{}) *Response {
-
-	r.setContentType("application/json")
-
 	jsn, err := json.Marshal(v)
 	if err != nil {
 		http.Error(r.w, err.Error(), http.StatusInternalServerError)
 		return r
 	}
 
-	return r.Send(jsn)
-}
-
-func (r *Response) setContentType(contentType string) {
-	if cty := r.w.Header().Get("Content-Type"); cty == "" {
-		r.w.Header().Set("Content-Type", contentType)
+	r.Set("Content-Type", "application/json; charset=utf-8")
+	_, err = r.w.Write(jsn)
+	if err != nil {
+		http.Error(r.w, err.Error(), http.StatusInternalServerError)
 	}
+
+	return r
 }
 
 // Send sends the HTTP response.
-// The body can be a string, a []byte, or any other value.
 func (r *Response) Send(body interface{}) *Response {
-	r.setContentType("application/octet-stream")
-
 	var data []byte
 	var err error
 
-	switch chunk := body.(type) {
+	switch v := body.(type) {
 	case string:
-		r.setContentType("text/html")
-		data = []byte(chunk)
+		r.Set("Content-Type", "text/plain; charset=utf-8")
+		data = []byte(v)
 	case []byte:
-		data = chunk
+		data = v
 	default:
-		r.setContentType("application/json")
-		data, err = json.Marshal(chunk)
-		if err != nil {
-			http.Error(r.w, err.Error(), http.StatusInternalServerError)
-			return r
-		}
+		return r.JSON(v)
 	}
 
-	if _, err := r.w.Write(data); err != nil {
+	_, err = r.w.Write(data)
+	if err != nil {
 		http.Error(r.w, err.Error(), http.StatusInternalServerError)
-		return r
 	}
 
-	r.status = http.StatusOK
 	return r
 }
 
 // Set sets the specified value to the HTTP response header field.
-// If the header is not already set, it creates the header with the specified value.
-func (r *Response) Set(key string, val ...string) {
-	if len(val) == 0 {
-		return
-	}
-
-	key = strings.ToLower(key)
-
-	if key == "content-type" && !strings.Contains(strings.ToLower(val[0]), "charset") {
-		if strings.HasPrefix(val[0], "text/") || strings.Contains(val[0], "application/json") {
-			val[0] += "; charset=utf-8"
+func (r *Response) Set(key string, value string) *Response {
+	key = http.CanonicalHeaderKey(key)
+	if key == "Content-Type" && !strings.Contains(value, "charset") {
+		if strings.HasPrefix(value, "text/") || strings.Contains(value, "application/json") {
+			value += "; charset=utf-8"
 		}
-
 	}
-
-	if len(val) > 1 {
-		r.w.Header().Set(key, strings.Join(val, ", "))
-	} else {
-		r.w.Header().Set(key, val[0])
-	}
+	r.w.Header().Set(key, value)
+	return r
 }
 
 // SendStatus sends the HTTP response status code.
 func (r *Response) SendStatus(statusCode int) *Response {
-	body := http.StatusText(statusCode)
-	if body == "" {
-		body = strconv.Itoa(statusCode)
-	}
-
-	r.w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	r.status = statusCode
+	r.Set("Content-Type", "text/plain; charset=utf-8")
 	r.w.WriteHeader(statusCode)
-	r.w.Write([]byte(body))
-
+	_, err := r.w.Write([]byte(http.StatusText(statusCode)))
+	if err != nil {
+		http.Error(r.w, err.Error(), http.StatusInternalServerError)
+	}
 	return r
 }
 
 // Status sets the HTTP status for the response.
 func (r *Response) Status(code int) *Response {
-	if r.written() {
-		return r
-	}
-	r.status = code
 	r.w.WriteHeader(code)
 	return r
 }
 
 // Get returns the HTTP response header specified by field.
 func (r *Response) Get(key string) string {
-	return r.w.Header().Get(key)
+	return r.w.Header().Get(http.CanonicalHeaderKey(key))
 }
 
 // Location sets the response Location HTTP header to the specified path parameter.
-// If the provided path is relative, it is instead resolved relative to the path of the current request.
-func (r *Response) Location(path string) string {
-	var location string
-
+func (r *Response) Location(path string) *Response {
 	if path == "back" {
-		location = r.ctx.request().Referer()
+		path = r.ctx.request().Referer()
 	}
 
-	if location == "" {
-		location = path
+	if path == "" {
+		path = "/"
 	}
 
-	parsedURL, err := url.Parse(location)
-	if err != nil {
-		// Log the error, replace with your logger
-		log.Println("Invalid URL: ", err)
-		location = "/"
-	}
-	r.w.Header().Set("Location", parsedURL.String())
-
-	return location
+	r.Set("Location", path)
+	return r
 }
 
 // Redirect redirects to the URL derived from the specified path, with specified status.
-// If status is not specified, status defaults to '302 Found'.
-func (r *Response) Redirect(path string, status ...int) {
-	statusCode := http.StatusFound // Default to 302
+func (r *Response) Redirect(path string, status ...int) *Response {
+	statusCode := http.StatusFound
 	if len(status) > 0 {
 		statusCode = status[0]
 	}
 
-	redirectURL := r.Location(path)
-	http.Redirect(r.w, r.ctx.request(), redirectURL, statusCode)
+	if path == "back" {
+		path = r.ctx.request().Referer()
+	}
+	if path == "" {
+		path = "/"
+	}
+
+	r.Location(path)
+	r.Status(statusCode)
+	return r
 }
 
 // Type sets the Content-Type HTTP header to the MIME type as determined by the filename’s extension.
-func (r *Response) Type(filename string) {
+func (r *Response) Type(filename string) *Response {
 	mimeType := mime.TypeByExtension(filepath.Ext(filename))
-	r.w.Header().Set("Content-Type", mimeType)
+	if mimeType == "" {
+		mimeType = "application/octet-stream"
+	}
+	r.Set("Content-Type", mimeType)
+	return r
 }
 
-// VaryFieldNameRegex checks for valid field names for the Vary header as defined by RFC 7231.
-var VaryFieldNameRegex = regexp.MustCompile(`^[!#$%&'*+\-.^_` + "`" + `|~0-9A-Za-z]+$`)
-
-// Vary add field to Vary header, if it doesn't already exist.
-func (r *Response) Vary(field string) {
-
+// Vary adds a field to the Vary header, if it doesn't already exist.
+func (r *Response) Vary(field string) *Response {
 	if field == "" {
 		log.Println("field argument is required")
-		return
+		return r
 	}
 
-	existingHeader := r.w.Header().Get("Vary")
-
-	fields := strings.Split(field, ",")
-	for i, field := range fields {
-		fields[i] = strings.TrimSpace(field)
+	existingHeader := r.Get("Vary")
+	if existingHeader == "*" || field == "*" {
+		r.Set("Vary", "*")
+		return r
 	}
 
-	existingFields := strings.Split(existingHeader, ",")
-	for i, field := range existingFields {
-		existingFields[i] = strings.TrimSpace(field)
-	}
-
+	fields := strings.Split(existingHeader, ",")
 	for _, f := range fields {
-		if !VaryFieldNameRegex.MatchString(f) {
-			log.Println("field argument contains an invalid header name")
-			return
+		if strings.TrimSpace(f) == field {
+			return r
 		}
 	}
 
-	if existingHeader == "*" || strings.Contains(field, "*") {
-		r.w.Header().Set("Vary", "*")
-		return
+	if existingHeader != "" {
+		field = existingHeader + ", " + field
 	}
-
-	// Utilizing a map to efficiently check if a field already exists
-	fieldMap := make(map[string]bool)
-	for _, f := range fields {
-		fieldMap[strings.ToLower(f)] = true
-	}
-
-	for _, ef := range existingFields {
-		if !fieldMap[strings.ToLower(ef)] {
-			fields = append(fields, ef)
-		}
-	}
-
-	r.w.Header().Set("Vary", strings.Join(fields, ", "))
+	r.Set("Vary", field)
+	return r
 }
 
 // Render renders a template with data and sends a text/html response.
-// name is the name of the template to render.
-func (r *Response) Render(name string, data interface{}) error {
-
-	temp := r.ctx.templates[name]
-	if temp == nil {
-		return fmt.Errorf("template %s not found", name)
+func (r *Response) Render(name string, data interface{}) *Response {
+	tmpl, ok := r.ctx.templates[name]
+	if !ok {
+		http.Error(r.w, fmt.Sprintf("template %s not found", name), http.StatusInternalServerError)
+		return r
 	}
-	return temp.Execute(r.w, data)
+
+	r.Set("Content-Type", "text/html; charset=utf-8")
+	err := tmpl.Execute(r.w, data)
+	if err != nil {
+		http.Error(r.w, err.Error(), http.StatusInternalServerError)
+	}
+	return r
 }
